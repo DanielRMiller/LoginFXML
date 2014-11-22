6c396e6c0b47a169a0df7b03ed6b594779ea135a
DanielRMiller <d.miller1.work@gmail.com>
2014-11-22 02:02:29 -0700

    Added the button and actionText to the bottom of the Login view.

--- LoginFXML/src/loginfxml/LoginController.java
+++ LoginFXML/src/loginfxml/LoginController.java
@@ -1,35 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package loginfxml;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 
 /**
  *
  * @author Xandron
  */
-public class LoginController implements Initializable {
+public class LoginController {
 
     @FXML
-    private Label label;
-
+    private Text actionText;
+    
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void handleSubmitButtonAction(ActionEvent event) {
+        actionText.setText("You clicked me!");
     }
-
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }
-
 }
